5fffd661010f372d7bf9f3395c0d07f25d4c7675
somename <someemail>
2017-06-09 20:28:04 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -28,9 +28,12 @@ public class BowlingGame {
 	}
 	
 	// Returns the game score
-	public int score(){
-		//to be implemented: should return game score
-		return 0;
+	public int score() throws BowlingException{
+		int score = 0;
+		for (int i = 0; i < frames.size(); i++) {
+			score = score + frames.get(i).score();
+		}
+		return score;
 	}
 
 	public List<Frame> getFrames() {
