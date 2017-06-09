d206ee3157c5750995ba6b5fc725aeb8eb61723a
somename <someemail>
2017-06-09 21:02:14 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -155,5 +155,32 @@ public class TestBowling {
 		assertEquals(14, game.score());
 		
 	}
+	
+	@Test
+	public void test_strikeIn10thFrame() throws BowlingException {
+		game = new BowlingGame();
+		frame = new Frame(3, 3);
+		Frame frame1 = new Frame(3, 3);
+		Frame frame2 = new Frame(3, 3);
+		Frame frame3 = new Frame(3, 3);
+		Frame frame4 = new Frame(3, 3);
+		Frame frame5 = new Frame(3, 3);
+		Frame frame6 = new Frame(3, 3);
+		Frame frame7 = new Frame(3, 3);
+		Frame frame8 = new Frame(3, 3);
+		Frame frame9 = new Frame(10, 0);
+		game.addFrame(frame1);
+		game.addFrame(frame);
+		game.addFrame(frame2);
+		game.addFrame(frame3);
+		game.addFrame(frame4);
+		game.addFrame(frame5);
+		game.addFrame(frame6);
+		game.addFrame(frame7);
+		game.addFrame(frame8);
+		game.addFrame(frame9);
+		
+		assertEquals(70, game.score());
+	}
 
 }
