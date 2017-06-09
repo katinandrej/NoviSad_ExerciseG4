4136c8e4d06b102a008e31da0bd8b910e621f087
somename <someemail>
2017-06-09 20:47:00 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,7 +9,7 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	private int score;
+	private int score = 0;
 	
 	public BowlingGame(){}
 	
@@ -24,13 +24,20 @@ public class BowlingGame {
 	}
 	
 	// Sets the bonus throws at the end of the game
-	public void setBonus(int firstThrow, int secondThrow) {
-		if (frames.size() == 10)
+	public void setBonus(int firstThrow, int secondThrow) throws BowlingException {
+		if (frames.size() == 10) {
+			if (frames.get(9).isStrike() && frames.get(8).isStrike()) {
+				score = score + 10 + firstThrow + firstThrow;
+			}
+		}
 	}
 	
 	public int addBonus() throws BowlingException {
 		for (int i = 0; i < frames.size(); i++) {
-			if (frames.get(i).isStrike() && i <= frames.size() - 1) {
+			if (frames.get(i).isStrike() && frames.get(i+1).isStrike() && i <= frames.size() - 1) {
+				score = score + 10 + frames.get(i+2).getFirstThrow() + frames.get(i+2).getFirstThrow() + frames.get(i+2).getSecondThrow();
+			}
+			else if (frames.get(i).isStrike() && i <= frames.size() - 1) {
 				score = score + frames.get(i+1).getFirstThrow() + frames.get(i+1).getSecondThrow();
 			}
 			else if (frames.get(i).isSpare() && i <= frames.size() - 1) {
